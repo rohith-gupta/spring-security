1bf571d099a79b62aad5998f6fc710e0d88a46f0
rohith-gupta <rmarri@my.bridgeport.edu>
2025-12-29 13:10:41 -0600

    multi user login using UserDetailsService.

--- spring-security/src/main/java/com/learning/spring_security/config/Customconfig.java
+++ spring-security/src/main/java/com/learning/spring_security/config/Customconfig.java
@@ -5,21 +5,35 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 
 @Configuration
 @EnableWebSecurity
 public class Customconfig {
-	
-	@Bean
-	public SecurityFilterChain securityFilterChain(HttpSecurity security)
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity security)
 	{
 		return security.authorizeHttpRequests(customizer -> customizer.anyRequest().authenticated())
 				.httpBasic(Customizer.withDefaults())
-				.formLogin(Customizer.withDefaults())
+//				.formLogin(Customizer.withDefaults())
 				.csrf(csrf -> csrf.disable())
 				.build();
+	}
+	
+	
+    @Bean
+	public UserDetailsService userDetailsService()
+	{
+		UserDetails rohith = User.withUsername("rohith").password("{noop}rohith").roles("USER").build();
+		UserDetails sharan = User.withUsername("sharan").password("{noop}mani").roles("USER").build();
+		
+		return new InMemoryUserDetailsManager(rohith,sharan);
 	}
 
 }
